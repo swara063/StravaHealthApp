eed399823767620b7b671079d0b78e6a1ea395b8
Swara Sameer Pawanekar <156062610+swara063@users.noreply.github.com>
2025-04-07 08:36:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ def refresh_token():
 
 # Start the server
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
